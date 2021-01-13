a863d209607cf3b5faac42ada34968e2f21afe9b
sia <anastasiya6956@mail.ru>
2021-01-13 12:42:39 +0300

    second task

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,37 @@
 let buttonShow = document.querySelector('button[data-action=show]');
 let buttonHide = document.querySelector('button[data-action=hide]');
 let list = document.querySelector('.list-container');
- 
+let li = document.querySelectorAll('li');
+
+
 document.addEventListener('click', function(event) {
     if (event.target.tagName != 'BUTTON') return;
-    
     if (event.target.tagName == 'BUTTON') {
-        if (event.target.dataset.action === 'show') {
-        buttonShow.classList.add('active');
-        buttonHide.classList.remove('active');
-        list.classList.add('show');
-        list.classList.remove('hide');
-        } else {
-        buttonShow.classList.remove('active');
-        buttonHide.classList.add('active');
-        list.classList.add('hide');
-        list.classList.remove('show');
-        } 
+        hide(event.target);
+        show(event.target);
     }
 })
 
+function hide(elem){
+    if (elem.dataset.action === 'hide') {
+        elem.classList.add('active');
+        buttonShow.classList.remove('active');
+        
+        for(item of li) {
+            if (item.firstElementChild.checked != true) {
+            item.classList.add('clean');
+            }
+        }     
+    }
+}
 
+function show(elem){
+    if (elem.dataset.action === 'show') {
+        elem.classList.add('active');
+        buttonHide.classList.remove('active');
+
+        for(item of li) {
+            item.classList.remove('clean');
+        }
+    }
+}  
